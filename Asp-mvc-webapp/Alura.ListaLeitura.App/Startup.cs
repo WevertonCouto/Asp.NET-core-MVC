a930ff1a0b123d5877557fcfa2ea11e6949bc785
Weverton Couto <wevertoncristais@gmail.com>
2020-01-30 13:47:17 -0300

    Router

--- Asp-mvc-webapp/Alura.ListaLeitura.App/Startup.cs
+++ Asp-mvc-webapp/Alura.ListaLeitura.App/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using Alura.ListaLeitura.App.Repositorio;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Alura.ListaLeitura.App
@@ -9,13 +10,39 @@ namespace Alura.ListaLeitura.App
     {
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(LivrosParaLer);
+            app.Run(Roteamento);
+        }
+
+        public Task Roteamento(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            var caminhos = new Dictionary<string, RequestDelegate>
+            {
+                { "/Livros/ParaLer", LivrosParaLer},
+                { "/Livros/Lendo", LivrosLendo},
+                { "/Livros/Lidos", LivrosLidos},
+            };
+
+            return (caminhos.ContainsKey(context.Request.Path)) ? caminhos[context.Request.Path].Invoke(context)
+            : context.Response.WriteAsync("Caminho inválido");
         }
 
         public Task LivrosParaLer(HttpContext context)
         {
             var _repo = new LivroRepositorioCSV();
             return context.Response.WriteAsync(_repo.ParaLer.ToString());
+        }
+
+        public Task LivrosLendo(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.Lendo.ToString());
+        }
+
+        public Task LivrosLidos(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.Lidos.ToString());
         }
     }
 }
